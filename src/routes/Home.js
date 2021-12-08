b3d88a2ab20ba4c1dc9c5a9cdaa55e11b4f299a3
yoonjong.park <yoonjongpark@gmail.com>
2021-12-08 21:57:40 +0900

    ADD getDocs for get nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  query,
+  getDocs,
+} from "firebase/firestore";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const q = query(collection(dbService, "nweets"));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach(doc => {
+      const nweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setNweets(prev => [nweetObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+
   const onSubmit = async e => {
     e.preventDefault();
     console.log(`서브밋 하는 느윗:${nweet}`);
@@ -20,6 +44,8 @@ const Home = () => {
     } = e;
     setNweet(value);
   };
+
+  console.log(nweets);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -32,6 +58,14 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+        {nweets &&
+          nweets.map(nweet => (
+            <div>
+              <h4>{nweet.nweet}</h4>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
